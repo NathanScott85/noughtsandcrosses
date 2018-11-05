231c7dccd53db6431ce61c7136482f3f76eadf6d
nathanscott85 <nathansfeely@gmail.com>
2018-11-05 14:57:06 +0000

    imported and braught in components into App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
-
+import Board from "./components/board";
+import Heading from "./components/Heading";
+import "./App.css";
 class App extends Component {
   render() {
-    return <main />;
+    return (
+      <main className="App">
+        <Heading name="Tic Tac Toe" />
+        <div className="container">
+          <Board />
+        </div>
+      </main>
+    );
   }
 }
 
